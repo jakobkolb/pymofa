de25149da2ef66b405d7fdb32d609c40da95a602
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2019-02-07 13:58:23 +0100

    version bump 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 # modifications in the installed package.
 
 setup(name="pymofa",
-      version="0.1.0",
+      version="0.2.0",
       description="to be added",
       url="to be added",
       author="Copan-group @ PIK",
